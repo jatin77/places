669ab09831ded7e9143a54413880eb5134258f15
jatin <jatin.rastogi@maximl.com>
2019-11-22 08:23:51 +0530

    django_heroku

--- discovery/settings.py
+++ discovery/settings.py
@@ -1,7 +1,7 @@
 
 
 import os
-
+import djang_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,3 +131,5 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+djang_heroku.settings(locals())
